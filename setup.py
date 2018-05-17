43cc3743b4ddad1090e0842dc1f1ac292cef9b03
Marek Cermak <macermak@redhat.com>
2018-05-17 10:13:09 +0200

    Changes to run tests with python3.6 on EPEL7
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   setup.py
    modified:   cico_run_pydoc.sh
    modified:   cico_run_pylint.sh
    modified:   check-docstyle.sh
    modified:   run-linter.sh

--- setup.py
+++ setup.py
@@ -36,8 +36,6 @@ setup(
 
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Utilities",
