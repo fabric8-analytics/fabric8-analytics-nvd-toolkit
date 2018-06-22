5e59a7cd27c27c20c2b910682cfb8a82057aeb96
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-22 11:14:10 +0200

    Fixed remaining shellcheck-related issues

--- run-tests.sh
+++ run-tests.sh
@@ -12,20 +12,20 @@ GREEN=$(tput bold && tput setaf 2)
 YELLOW=$(tput bold && tput setaf 3)
 
 function prepare_venv() {
-		# we want tests to run on python3.6
-	printf 'checking alias `python3.6` ... '
+	# we want tests to run on python3.6
+	printf 'checking alias `python3.6` ... ' >&2
 	PYTHON=$(which python3.6 2> /dev/null)
 	if [ "$?" -ne "0" ]; then
-		printf "${YELLOW} NOT FOUND ${NORMAL}\n"
+		printf "%sNOT FOUND%s\n" "${YELLOW}" "${NORMAL}" >&2
 
-		printf 'checking alias `python3` ... '
+		printf 'checking alias `python3` ... ' >&2
 		PYTHON=$(which python3 2> /dev/null)
 
 		let ec=$?
 		[ "$ec" -ne "0" ] && printf "${RED} NOT FOUND ${NORMAL}\n" && return $ec
 	fi
 
-	printf "${GREEN} OK ${NORMAL}\n"
+	printf "%sOK%s\n" "${GREEN}" "${NORMAL}" >&2
 
 	${PYTHON} -m venv "venv" && source venv/bin/activate
 }
