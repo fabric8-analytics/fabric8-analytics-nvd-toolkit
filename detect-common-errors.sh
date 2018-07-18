09167753570b5866cfa63acb3f0375d4875e3755
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-18 15:19:09 +0200

    Fix virtual environment setup for CI

--- detect-common-errors.sh
+++ detect-common-errors.sh
@@ -6,14 +6,29 @@ separate_files="setup.py"
 pass=0
 fail=0
 
-function prepare_venv() {
-    VIRTUALENV=$(which virtualenv)
-    if [ $? -eq 1 ]; then
-        # python34 which is in CentOS does not have virtualenv binary
-        VIRTUALENV=$(which virtualenv-3)
-    fi
+# set up terminal colors
+NORMAL=$(tput sgr0)
+RED=$(tput bold && tput setaf 1)
+GREEN=$(tput bold && tput setaf 2)
+YELLOW=$(tput bold && tput setaf 3)
 
-    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install pyflakes
+function prepare_venv() {
+	# we want tests to run on python3.6
+	printf 'checking alias `python3.6` ... ' >&2
+	PYTHON=$(which python3.6 2> /dev/null)
+	if [ "$?" -ne "0" ]; then
+		printf "%sNOT FOUND%s\n" "${YELLOW}" "${NORMAL}" >&2
+
+		printf 'checking alias `python3` ... ' >&2
+		PYTHON=$(which python3 2> /dev/null)
+
+		let ec=$?
+		[ "$ec" -ne "0" ] && printf "${RED} NOT FOUND ${NORMAL}\n" && return $ec
+	fi
+
+	printf "%sOK%s\n" "${GREEN}" "${NORMAL}" >&2
+
+    ${PYTHON} -m venv "venv" && source venv/bin/activate && pip install pyflakes
 }
 
 # run the pyflakes for all files that are provided in $1
