a4175ab1be1c2b6bfbb15c0c7fd2fbfe7d13c41d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-27 10:22:40 +0200

    Check Python version in test CI job

--- run-tests.sh
+++ run-tests.sh
@@ -11,6 +11,10 @@ RED=$(tput bold && tput setaf 1)
 GREEN=$(tput bold && tput setaf 2)
 YELLOW=$(tput bold && tput setaf 3)
 
+check_python_version() {
+    python3 tools/check_python_version.py 3 6
+}
+
 function prepare_venv() {
 	# we want tests to run on python3.6
 	printf 'checking alias `python3.6` ... ' >&2
@@ -30,9 +34,9 @@ function prepare_venv() {
 	${PYTHON} -m venv "venv" && source venv/bin/activate
 }
 
+check_python_version
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
-
 
 # install nvdlib
 git clone https://github.com/msrb/nvdlib
