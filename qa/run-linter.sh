450c8ce0afe9f5b4ca8435f68f57aac38ecfcba8
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-27 09:54:41 +0200

    Updated the script run-linter.sh to be able to run from qa subdirectory

--- qa/run-linter.sh
+++ qa/run-linter.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Script to check all Python scripts for PEP-8 issues
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 IFS=$'\n'
 
 # list of directories with sources to check
-directories=$(cat directories.txt)
+directories=$(cat ${SCRIPT_DIR}/directories.txt)
 
 # list of separate files to check
-separate_files=$(cat files.txt)
+separate_files=$(cat ${SCRIPT_DIR}/files.txt)
 
 pass=0
 fail=0
@@ -37,6 +41,8 @@ function prepare_venv() {
 
 	${PYTHON} -m venv "venv" && source venv/bin/activate && pip install pycodestyle >&2
 }
+
+pushd "${SCRIPT_DIR}/.."
 
 
 echo "----------------------------------------------------"
@@ -98,3 +104,5 @@ else
     echo "Linter fail, $fail source files out of $total source files need to be fixed"
     exit 1
 fi
+
+popd
